6208b2b5c09a0cdda7db24316e196747fed62720
Maulana-SH <maulana.hidayat@centralmegakencana.com>
2026-06-19 08:35:04 +0700

    Fix/main project (#34)
    * fix refrences
    
    * fix: remove duplicate PCSC reference from Lib, keep NuGet version only
    
    * feat: change main form
    
    ---------
    
    Co-authored-by: Arief <arief@skkjewels.com>
    Co-authored-by: Maulana S H <119746645+itdevskk24@users.noreply.github.com>

--- SKK/My Project/Application.Designer.vb
+++ SKK/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SKK.xrFRMLogin
+            Me.MainForm = Global.SKK.xf39GanChart
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
